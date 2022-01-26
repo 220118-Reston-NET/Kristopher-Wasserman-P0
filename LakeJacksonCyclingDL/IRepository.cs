9c8403fa8ba39379c2a079ed5fb331daa97e9592
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-01-26 16:37:34 -0600

    new

---
+++ LakeJacksonCyclingDL/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using LakeJacksonCyclingModel;
+
+namespace LakeJacksonCyclingDL
+{
+    public interface IRepository
+    {
+           ItemsLines AddProduct( ItemsLines p_item);
+           Customers AddCustomer( Customers p_name);
+           /// <summary>
+            ///     Gets the current list of customers within the database.
+            /// </summary>
+            /// <returns></returns>
+           // List<Customers> GetCustomers();
+    }
+}
+
