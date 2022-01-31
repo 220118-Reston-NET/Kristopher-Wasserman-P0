941a0195f5ec6308f093b74185eaeb8346bbbfb0
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-01-31 13:32:41 -0600

    add new function, Products can now be added

--- LakeJacksonCyclingDL/IRepository.cs
+++ LakeJacksonCyclingDL/IRepository.cs
@@ -4,7 +4,7 @@ namespace LakeJacksonCyclingDL
 {
     public interface IRepository
     {
-           ItemsLines AddProduct( ItemsLines p_item);
+           ItemsLines AddProduct( ItemsLines p_name);
            Customers AddCustomer( Customers p_name);
            /// <summary>
             ///     Gets the current list of customers within the database.
