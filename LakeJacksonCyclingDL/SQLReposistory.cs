a7335c53aee989171f23693742495c43501e553d
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-02-02 13:36:19 -0600

    new sql database statements added

---
+++ LakeJacksonCyclingDL/SQLReposistory.cs
@@ -0,0 +1,27 @@
+using LakeJacksonCyclingModel;
+
+namespace LakeJacksonCyclingDL
+{
+    public class SQLRepository : IRepository
+    {
+        public Customers AddCustomer(Customers p_name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ItemsLines AddProduct(ItemsLines p_name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customers> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ItemsLines> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
